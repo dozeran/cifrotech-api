1805434d660b007609b16a366a094860d91dec86
dozeran <dozeran@proton.me>
2024-03-20 18:38:45 +0200

    request limit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const bodyParser = require("body-parser");
 require("./db");
 const app = require("./app");
 
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log("Server running. Use our API on port: 3000");
 });
